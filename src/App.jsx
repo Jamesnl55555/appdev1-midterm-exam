dcebb50ea697ca8ed7ed790b09127f0783ed6aef
Jamesnl55555 <jamesnl55555@gmail.com>
2025-10-04 10:29:15 +0800

    added files and modified App.jsx to import photos, also modified it to become my portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import sidepic from "./2110_character/img/sidepic.jpg";
+import profilepic from "./2110_character/img/jimmy.jpg";
+import name from "./2110_character/img/name.jpg";
 function App() {
   return (
     <>
@@ -7,10 +10,13 @@ function App() {
           data-desktop-seq-no={1}
           data-mobile-seq-no={1}
         >
+          <header>
+            <h2 className="name" >Jimmy N. Lumaad</h2>
+          </header>
           <p className="tm-hero-text">
-            “Class aptent taciti sociosqu ad litora torquent per conubia nostra, per
-            inceptos himenaeos. Maecenas vel lacinia ipsum, nec fermentum diam.
-            Nulla nec gravida odio, eget vestibulum urna.”
+            “A student aiming to be a great professional and is 
+            passionate at accumulating knowledge regarding computer programming."
+
           </p>
           <header className="tm-block-brand">
             <div className="tm-bg-primary-dark tm-text-white tm-block-brand-inner">
@@ -20,27 +26,31 @@ function App() {
           </header>
         </div>
         <div className="item" data-desktop-seq-no={2} data-mobile-seq-no={4}>
-          <img src="img/image-01.jpg" alt="Image" className="tm-img-left" />
+          <img alt="Image" className="tm-img-left" src={sidepic} />
         </div>
+        
         <div
           className="item tm-bg-secondary tm-text-white tm-block tm-block-wider tm-block-pad tm-block-left-2"
           data-desktop-seq-no={3}
           data-mobile-seq-no={5}
         >
           <i className="fas fa-award fa-4x tm-block-icon" />
-          <p>
-            Maecenas scelerisque ex neque, vel ultrices purus pharetra sit amet.
-            Donec consectetur ipsum in elit eleifend porta. Morbi bibendum porttitor
-            dui. Proin molestie purus non nisi blandit rutrum.
+          <header>
+              <h2 className="tm-text-uppercase"> Educational Acheivements:</h2>
+          </header>
+          <h4 className="tm-text-uppercase"> 2015 - 2025</h4>
+          <ul>
+              <li>RECEIVED A DSR LOYALTY AWARD
+              CERTIFICATE.</li>
+              <li>RECEIVED “WITH HONOR”
+              CERTIFICATES IN SOME YEARS.</li>
+              <li>BECAME A "DEAN'S LISTER" IN COLLEGE</li>
+              
+          </ul>
+          <p className="tm-mt tm-mb-small">
+            These are some of my educational achievements that I am proud of.
           </p>
-          <div className="tm-text-right">
-            <a href="#" className="tm-btn tm-btn-small tm-btn-primary tm-mt">
-              Read More
-            </a>
-          </div>
-        </div>
-        <div className="item" data-desktop-seq-no={4} data-mobile-seq-no={8}>
-          <img src="img/image-04.jpg" alt="Image" className="tm-img-left" />
+          {/* */}
         </div>
         <div
           className="tm-footer"
@@ -48,19 +58,9 @@ function App() {
           data-desktop-seq-no={5}
           data-mobile-seq-no={9}
         >
-          <img src="img/qr-link-tooplate.png" alt="QR Code" className="tm-img-qr" />
-          <div>
-            <p className="tm-mb-small">Copyright © 2018 Company Name</p>
-            <p>
-              Designed by{" "}
-              <a rel="nofollow" href="https://www.facebook.com/tooplate">
-                Tooplate
-              </a>
-            </p>
-          </div>
         </div>
         <div className="item" data-desktop-seq-no={6} data-mobile-seq-no={2}>
-          <img src="img/image-02.jpg" alt="Image" />
+          <img src={profilepic} alt="Image" />
         </div>
         <div
           className="item tm-block-right"
@@ -69,86 +69,46 @@ function App() {
         >
           <div className="tm-block-right-inner tm-bg-primary-light tm-text-white tm-block tm-block-wider tm-block-pad">
             <header>
-              <h2 className="tm-text-uppercase">Proin Molestie Purus Non</h2>
+              <h2 className="tm-text-uppercase">Hobbies:</h2>
             </header>
-            <p>
-              You can freely use this Character HTML Template for your site. Please{" "}
-              <a href="https://www.facebook.com/tooplate">follow us</a> on Facebook
-              page for updates. Don't forget to tell your friends about Tooplate.
-              Thank you. :)
-            </p>
+            <ul>
+              <li>Playing Chess</li>
+              <li>Reading Books</li>
+              <li>Traveling</li>
+              <li>Listening to music</li>
+              <li>Watching Movies</li>
+              <li>Playing Video Games</li>
+            </ul>
+            <header>
+              <h2 className="tm-text-uppercase">Skills:</h2>
+            </header>
+            <ul>
+              <li>Basic C++ programming</li>
+              <li>Basic HTML programming</li>
+              <li>Basic PHP programming</li>
+              <li>Basic Python programming</li>
+              <li>Basic Javascript programming</li>
+              <li>Basic SQL programming</li>
+              <li>Basic C# programming</li>
+              <li>Basic English, Tagalog, and Bisaya speaking</li>
+            </ul>
             <p className="tm-mt tm-mb-small">
-              Aenean gravida augue luctus, egestas massa ac, hendrerit ipsum.
-              Vestibulum et ex sollicitudin, commodo liqula suscipit, laoreet lacus.
+              These are my hobbies and skills that I have developed over the years.
             </p>
             {/* */}
           </div>
         </div>
-        <div className="item" data-desktop-seq-no={8} data-mobile-seq-no={6}>
-          <img src="img/image-03.jpg" alt="Image" />
+        <div className="item shrink" data-desktop-seq-no={8} data-mobile-seq-no={6}>
+          <img src={name} alt="Image" />
         </div>
-        <div
-          className="item tm-bg-white tm-block tm-form-section"
-          data-desktop-seq-no={9}
-          data-mobile-seq-no={7}
-        >
-          <div className="tm-form-container tm-block-pad tm-pb-0">
-            <header>
-              <h2 className="tm-text-uppercase tm-text-gray-light tm-mb">
-                Contact Us
-              </h2>
-            </header>
-            <form action="index.html" className="tm-contact-form" method="POST">
-              <div className="tm-form-group">
-                <input
-                  type="text"
-                  id="contact_name"
-                  name="contact_name"
-                  className="form-control"
-                  placeholder="Name"
-                  required=""
-                />
-              </div>
-              <div className="tm-form-group">
-                <input
-                  type="email"
-                  id="contact_email"
-                  name="contact_email"
-                  className="form-control"
-                  placeholder="Email"
-                  required=""
-                />
-              </div>
-              <div className="tm-form-group">
-                <textarea
-                  rows={5}
-                  id="contact_message"
-                  name="contact_message"
-                  className="form-control"
-                  placeholder="Message"
-                  required=""
-                  defaultValue={""}
-                />
-              </div>
-              <div className="tm-text-right">
-                <button
-                  type="submit"
-                  className="tm-btn tm-btn-secondary tm-btn-pad-big"
-                >
-                  Send
-                </button>
-              </div>
-            </form>
-          </div>
+        <div>
           <div className="tm-form-section-tag">
             <div className="tm-bg-secondary tm-text-white tm-block-pad tm-form-section-tag-inner">
               <header>
-                <h2>Proin imperdiet commodo nisi</h2>
+                <h2>Motto:</h2>
               </header>
               <p>
-                Mauris sodales vulputate ante a dapibus Donec vitae maximus dolor,
-                pharetra imperdiet lectus. Praesent pharetra elit ac est congue
-                volutpat.
+                "Acheiving greatness is what people dream of, we should strive for it instead of just dreaming about it."
               </p>
             </div>
           </div>
